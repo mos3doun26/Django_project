01be51c06011100aed72f215085e48e9118a2e5b
mohamed <mos3doun26@gmail.com>
2024-09-07 10:08:17 +0300

    Add profile model, And Some new feautres on profile sect: automated profile creation, saving and profile image

--- users/models.py
+++ users/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    ...
-    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default="profile_pics/defualt.jpeg", upload_to="profile_pics")
+
+    def __str__(self):
+        return f"{self.user.username}"
+
