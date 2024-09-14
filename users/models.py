590066dd11beed4a215a3842e6855e0baebad365
mohamed <mos3doun26@gmail.com>
2024-09-14 07:59:53 +0300

    igonring some files

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
@@ -8,6 +9,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
-
     
-
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
